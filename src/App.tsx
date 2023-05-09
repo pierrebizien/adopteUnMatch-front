44b0299c9b6bd525376363ac91afbaceeaaef46d
Pierre BIZIEN <pierre.bizien@edu.escp.eu>
2023-05-09 16:11:25 +0200

    passage au TS

--- src/App.tsx
+++ src/App.tsx
@@ -22,11 +22,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Test
+        Click on the Vite and React logos to learn more
       </p>
     </>
   )
